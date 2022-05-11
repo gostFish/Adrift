882b8762a569d0f597406f4ec6df4e48790a1f55
Malcolm Grech <malcolm.grech.18@um.edu.mt>
2022-05-11 21:19:11 +0200

    Updated Intro animation

--- Assets/Scripts/UI/IntroAnimation.cs
+++ Assets/Scripts/UI/IntroAnimation.cs
@@ -4,13 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class IntroAnimation : MonoBehaviour
 {
+    public GameObject fadeObj;
+    public CanvasGroup fadeUI;
+
     public GameObject sceneManager;
     public GameObject mainCamera;
     public Texture[] frames;
     public float changeTiming;
     public float fadeTiming;
     public float time;
-    public float fadeTime;
+    //public float fadeTime;
 
     private AudioSource audioSource;
     public AudioClip thunder;
@@ -21,47 +24,68 @@ public class IntroAnimation : MonoBehaviour
     void Start()
     {
         played = false;
-        time = 0;
-        fadeTime = 0;
+        time = 2;
+        //fadeTime = 0;
         audioSource = mainCamera.GetComponent<AudioSource>();
     }
     void FixedUpdate()
     {
         time = time + Time.deltaTime;
-        fadeTime = fadeTime + Time.deltaTime / (fadeTiming);
+        //fadeTime = fadeTime + Time.deltaTime / (fadeTiming);
         
         //Debug.Log("time: " + time + " fadeTime: " + fadeTime + " Lerp: " + Mathf.Lerp(0, 255, time/fadeTiming));
         switch (time)
         {
             case float time when time < changeTiming:
-                
-                
-                if(time < changeTiming - fadeTiming)
+                //Debug.Log("Here");
+                //fadeObj.SetActive(true);
+
+                GetComponent<RawImage>().texture = frames[0];
+
+                if (time > (changeTiming - fadeTiming))
                 {
-                    GetComponent<RawImage>().texture = frames[0];
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - (changeTiming - fadeTiming)) / fadeTiming);
                 }
-                else
-                {                    
-                    //lerpVal = (int)(Mathf.Lerp(255, 0, (time - fadeTiming) / fadeTiming));
-                    //Debug.Log("Lerp = " + lerpVal);
-                    //GetComponent<RawImage>().color = new Color(255, 255, 255, lerpVal);
-                }                
-                    //GetComponent<RawImage>().color = new Color(255, 255, 255, Mathf.Lerp(255, 0, time  /  (changeTiming - fadeTime)));    
                 break;
             case float time when time < (changeTiming * 2):
+                if (time < (changeTiming + fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(1, 0, ((time - (changeTiming)) / fadeTiming));
+                }
                 GetComponent<RawImage>().texture = frames[1];
+                if (time > ((changeTiming*2) - fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - ((changeTiming*2) - fadeTiming)) / fadeTiming);
+                }
                 break;
             case float time when time < (changeTiming * 3):
+                if (time < ((changeTiming*2) + fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(1, 0, ((time - (changeTiming * 2)) / fadeTiming));
+                }
                 GetComponent<RawImage>().texture = frames[2];
+                if (time > ((changeTiming * 3) - fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - ((changeTiming * 3) - fadeTiming)) / fadeTiming);
+                }
                 break;
             case float time when time < (changeTiming * 4):
+                if (time < ((changeTiming * 3) + fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(1, 0, ((time - (changeTiming * 3)) / fadeTiming));
+                }
                 GetComponent<RawImage>().texture = frames[3];
+                if (time > ((changeTiming * 4) - fadeTiming))
+                {
+                    time = time + 2;
+                }
                 break;
             case float time when time < (changeTiming * 5):
                 
                 switch (time)
                 {
                     case float time2 when time < (changeTiming * 4+0.03):
+                        fadeUI.alpha = 0;
                         if (!played)
                         {
                             audioSource.PlayOneShot(thunder);
@@ -85,14 +109,35 @@ public class IntroAnimation : MonoBehaviour
                         GetComponent<RawImage>().texture = frames[4];
                         break;
                 }
+                if (time > ((changeTiming * 5) - fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - ((changeTiming * 5) - fadeTiming)) / fadeTiming);
+                }
                 break;
             case float time when time < (changeTiming * 6):
+                if (time < ((changeTiming * 5) + fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(1, 0, ((time - (changeTiming * 5)) / fadeTiming));
+                }
                 GetComponent<RawImage>().texture = frames[5];
+                if (time > ((changeTiming * 6) - fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - ((changeTiming * 6) - fadeTiming)) / fadeTiming);
+                }
                 break;
             case float time when time < (changeTiming * 7):
+                if (time < ((changeTiming * 6) + fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(1, 0, ((time - (changeTiming * 6)) / fadeTiming));
+                }
                 GetComponent<RawImage>().texture = frames[6];
+                if (time > ((changeTiming * 7) - fadeTiming))
+                {
+                    fadeUI.alpha = Mathf.Lerp(0, 1, (time - ((changeTiming * 7) - fadeTiming)) / fadeTiming);
+                }
                 break;
             case float time when time< (changeTiming* 9):
+                //fadeObj.SetActive(false);
                 SceneManager.LoadScene("Level");
                 break;
 
@@ -100,7 +145,7 @@ public class IntroAnimation : MonoBehaviour
     }
     public void StartCutscene()
     {
-        time = 0;
+        time = 2;
     }
     public void Skip()
     {
